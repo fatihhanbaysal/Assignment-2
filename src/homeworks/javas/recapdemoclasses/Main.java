52aa274918ae1a11b90085f3ccf1e7b03770519b
fatihhanbaysal <fatihhanbaysal@hotmail.com>
2022-09-28 01:57:09 +0300

    homework2 commit 3

---
+++ src/homeworks/javas/recapdemoclasses/Main.java
@@ -0,0 +1,19 @@
+package javas.recapdemoclasses;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        int result = calc.add(3, 4);
+        int result1 = calc.sub(5, 1);
+        int result2 = calc.mul(7, 8);
+        int result3 = calc.div(9, 10);
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+
+    }
+
+}
